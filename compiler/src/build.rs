625b076725c18ab845f90ceb03aa9cd57b9781e4
R1tschY <r1tschy@posteo.de>
2021-01-24 00:03:46 +0100

    Write files only when changed

--- compiler/src/build.rs
+++ compiler/src/build.rs
@@ -1,13 +1,29 @@
-use crate::generate::generate;
-use crate::moc::MocConfig;
-use crate::qobject::QObjectConfig;
-use crate::CcBuild;
 use std::error::Error;
 use std::path::{Path, PathBuf};
 use std::{fs, io};
 
+use crate::generate::generate;
+use crate::moc::MocConfig;
+use crate::qobject::QObjectConfig;
+use crate::CcBuild;
+
 fn invalid_arg(message: &'static str) -> io::Error {
     io::Error::new(io::ErrorKind::InvalidInput, message)
+}
+
+fn write_when_changed(path: &Path, content: &str) -> io::Result<bool> {
+    let old_content = match fs::read_to_string(path) {
+        Ok(old_content) => Some(old_content),
+        Err(err) if err.kind() == io::ErrorKind::NotFound => None,
+        Err(err) => return Err(err),
+    };
+
+    if old_content.as_ref().map(|c| c as &str) != Some(content) {
+        fs::write(path, content)?;
+        Ok(true)
+    } else {
+        Ok(false)
+    }
 }
 
 pub fn build(
@@ -28,20 +44,20 @@ pub fn build(
 
     // Generate
     let (cpp_code, rust_code) = generate(&moc_name, &[obj]);
-    // TODO: check content before writing
-    fs::write(path, &cpp_code).unwrap();
-    fs::write(rs_path, &rust_code).unwrap();
+    let cpp_changed = write_when_changed(path, &cpp_code)?;
+    write_when_changed(&rs_path, &rust_code)?;
 
-    // MOC
-    moc.build(path, &moc_path).unwrap();
+    if cpp_changed {
+        // MOC
+        moc.build(path, &moc_path)?;
+    }
 
     // C++
     cpp.clone()
         .cpp(true)
         .file(path)
         .static_flag(true)
-        .try_compile(output_name)
-        .unwrap();
+        .try_compile(output_name)?;
 
     Ok(())
 }
